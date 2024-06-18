abbf7a3449ac707a82e765fe0ffd9fae8d92639e
Md.Jahid Hasan <jahidhasan5803733@gmail.com>
2024-06-18 13:33:44 +0600

    Done integrate html template

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+
+@include('home.css')
+
+<body>
+  <div class="hero_area">
+    
+    <!-- header section strats -->
+    <header class="header_section">
+      @include('home.header')
+    </header>
+    <!-- end header section -->
+
+    <!-- slider section -->
+    @include('home.slider')
+    <!-- end slider section -->
+  </div>
+  <!-- end hero area -->
+
+  <!-- shop section -->
+  @include('home.shop')
+  <!-- end shop section -->
+
+
+  <!-- contact section -->
+
+  @include('home.contact')
+
+  <br><br><br>
+
+  <!-- end contact section -->
+
+  <!-- info section -->
+@include('home.footer')
+
+  <!-- end info section -->
+  <script src="{{asset('js/jquery-3.4.1.min.js')}}"></script>
+  <script src="{{asset('js/bootstrap.js')}}"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js">
+  </script>
+  <script src="{{asset('js/custom.js')}}"></script>
+
+</body>
+</html>
